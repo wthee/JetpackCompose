a73e5e8dd33bf4a871703a8913781cb701396fb6
wthee <secretzqw@gmail.com>
2021-02-23 17:09:14 +0800

    添加登录页面

--- sample/src/main/java/cn/wthee/jetpackcompose/ui/login/LoginPage.kt
+++ sample/src/main/java/cn/wthee/jetpackcompose/ui/login/LoginPage.kt
@@ -1,13 +1,18 @@
 package cn.wthee.jetpackcompose.ui.login
 
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.ExperimentalAnimationApi
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.KeyboardArrowRight
+import androidx.compose.material.icons.filled.Lock
+import androidx.compose.material.icons.filled.Person
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
@@ -15,6 +20,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.navigation.NavController
 import cn.wthee.jetpackcompose.R
 import cn.wthee.jetpackcompose.nav.toHome
@@ -25,6 +31,7 @@ import cn.wthee.jetpackcompose.viewmodel.CommonViewModel
 /**
  * 登录页面
  */
+@ExperimentalAnimationApi
 @Composable
 fun LoginPage(commonViewModel: CommonViewModel, navController: NavController) {
     val userName = rememberSaveable { mutableStateOf("") }
@@ -44,6 +51,8 @@ fun LoginPage(commonViewModel: CommonViewModel, navController: NavController) {
             label = { Text("用户名") },
             modifier = Modifier.fillMaxWidth().padding(Dimen.medium),
             backgroundColor = MaterialTheme.colors.surface,
+            leadingIcon = { Icon(Icons.Filled.Person, null) },
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Text),
             shape = shapes.small,
             onValueChange = {
                 userName.value = it
@@ -54,17 +63,22 @@ fun LoginPage(commonViewModel: CommonViewModel, navController: NavController) {
             label = { Text("密码") },
             modifier = Modifier.fillMaxWidth().padding(Dimen.medium),
             backgroundColor = MaterialTheme.colors.surface,
+            leadingIcon = { Icon(Icons.Filled.Lock, null) },
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password),
             shape = shapes.small,
             onValueChange = {
                 psw.value = it
             })
 
-        Button(onClick = {
-            //TODO 登录校验，登录成功 appbar 显示为用户名
-            toHome(commonViewModel, navController, "")
-        }, modifier = Modifier.padding(top = Dimen.large)) {
-            Text(stringResource(id = R.string.menu_login))
-            Icon(Icons.Filled.KeyboardArrowRight, null)
+        AnimatedVisibility(visible = userName.value.isNotEmpty() && psw.value.isNotEmpty()) {
+            Button(onClick = {
+                //TODO 登录校验，登录成功 appbar 显示为用户名
+                toHome(commonViewModel, navController, "")
+            }, modifier = Modifier.padding(top = Dimen.large)) {
+                Text(stringResource(id = R.string.menu_login))
+                Icon(Icons.Filled.KeyboardArrowRight, null)
+            }
         }
+
     }
 }
